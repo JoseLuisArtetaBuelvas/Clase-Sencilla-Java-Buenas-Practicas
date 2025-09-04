26fa1baeec4a9b6e00b9808d49b163e966311b6d
Zadu <jartetab@unicartagena.edu.co>
2025-09-04 13:00:45 -0500

    Firt commit

---
+++ src/main/java/com/mycompany/buenaspracticas/BuenasPracticas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.buenaspracticas;
+
+/**
+ *
+ * @author zaduking
+ */
+public class BuenasPracticas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
