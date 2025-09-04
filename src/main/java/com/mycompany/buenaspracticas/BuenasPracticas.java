8d3c27beaa233f6dca06acd3467760a44da9a90d
Zadu <jartetab@unicartagena.edu.co>
2025-09-04 13:14:13 -0500

    Second commit

--- src/main/java/com/mycompany/buenaspracticas/BuenasPracticas.java
+++ src/main/java/com/mycompany/buenaspracticas/BuenasPracticas.java
@@ -9,8 +9,31 @@ package com.mycompany.buenaspracticas;
  * @author zaduking
  */
 public class BuenasPracticas {
-
+    private String nombre;
+    private int edad;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Hacemos un trycath para atrapar posibles errores
+        try {
+            BuenasPracticas persona1 = new BuenasPracticas("Jose", 21);
+            BuenasPracticas persona2 = new BuenasPracticas("Luis", 21);
+            
+            //Mostramos en consola los resultados
+            System.out.println("Datos guardados correctamte");
+            System.out.println(persona1.nombre + persona1.edad);
+            System.out.println(persona2.nombre + persona2.edad);
+        } catch (Exception e) {
+        }
+    }
+    
+    public BuenasPracticas(String nombre, int edad){
+        if(nombre == null || nombre.trim().isEmpty()){
+            throw new IllegalArgumentException("El nombre se encuentra vacio");
+        }else if(edad < 0 || edad >= 150){
+            throw new IllegalArgumentException("Error en tu edad" + " " + edad);
+        }
+        
+        //inicializacion de las instancias de atributos
+        this.nombre = nombre.trim();
+        this.edad = edad;
     }
 }
